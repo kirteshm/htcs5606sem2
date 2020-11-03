c91e1de84b48e93b382d355f10865e1425ac8622
kirteshm <mehtak06@myunitec.ac.nz>
2020-11-04 08:43:57 +1300

    I created a html page in php

---
+++ htmlcode.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HTML Code</title>
+</head>
+<body>
+<h1>This is HTML way to write php</h1>
+<p>This is where the paragraph goes</p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,3 +2,5 @@
 
 echo "<h1>HTCS5606</h1>";
 echo "<p>Hello all</p>";
+
+?>
