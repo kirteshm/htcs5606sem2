781310e47911fc099387c529bfb3baf0625949d1
kirteshm <mehtak06@myunitec.ac.nz>
2020-11-04 10:16:39 +1300

    my first conn to db

---
+++ phpAndDatabase/firstDBconnection.php
@@ -0,0 +1,31 @@
+<?php
+
+// Create a database connection
+
+    $server = "c584md9egjnm02sk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbusername = "h1r174kgefa2bwv1";
+    $dbpassword = "nz1tdqgzvtixcxel";
+    $dbname = "dry1psur1w11ayhb";
+
+    $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+
+    if($conn->error){
+        echo $conn->error;
+    }
+    else{
+        echo "connected";
+    }
+
+// write a query
+
+    $sql = "select * from users";
+
+// execute the query
+
+    $result = mysqli_query($conn, $sql);
+
+// show my result
+
+    while ($row = $result->fetch_assoc()){
+        echo $row["id"];
+    }
