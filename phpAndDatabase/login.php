b8bab7a98dd9318e14c7a271f22ec5bfa11b447b
kirteshm <mehtak06@myunitec.ac.nz>
2020-11-05 10:26:40 +1300

    Created a database connection for login

--- phpAndDatabase/login.php
+++ phpAndDatabase/login.php
@@ -3,5 +3,40 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 $select = $_POST["mySelect"];
 
-echo "Username: ".$username;
-echo "<br>Password: ".$password;
+echo "Username: " . $username;
+echo "<br>Password: " . $password;
+
+//create database connection
+
+$server = "c584md9egjnm02sk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbusername = "h1r174kgefa2bwv1";
+$dbpassword = "nz1tdqgzvtixcxel";
+$dbname = "dry1psur1w11ayhb";
+
+$conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+
+if ($conn->error) {
+    echo $conn->error;
+} else {
+    echo "connected";
+}
+// create a query
+$sql = "select * from users where username = " . $username . " and password = " . $password;
+
+//run a query
+$result = mysqli_query($conn, $sql);
+
+// show my result
+
+if ($result->num_rows == 1) {
+    echo "You have Login";
+    while ($row = $result->fetch_assoc()) {
+        echo $row["firstname " . "lastname"];
+    }
+} else {
+    echo "Wrong username or password";
+}
+
+while ($row = $result->fetch_assoc()) {
+    echo $row["firstname"];
+}
