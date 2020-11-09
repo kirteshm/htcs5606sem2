ae4567d99fa4b6827d9f1446d8258088b132db7c
kirteshm <mehtak06@myunitec.ac.nz>
2020-11-10 09:05:10 +1300

    Include-index page and menu page

--- include/page1.php
+++ include/page1.php
@@ -1,6 +1,5 @@
-
 <ul>
-    <li><a href="index.php">home</a></li>
+    <li><a href="index.php">Home</a></li>
     <li><a href="aboutUs.php">About us</a></li>
     <li><a href="products.php">Products</a></li>
 </ul>
