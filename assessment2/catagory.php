f01372b2d90eb90c373c30d4ecbd8af08b80df6b
kirteshm <mehtak06@myunitec.ac.nz>
2020-11-16 08:57:45 +1300

    catagory php added

---
+++ assessment2/catagory.php
@@ -0,0 +1,21 @@
+<?php
+//1. connect to database
+$server = "c584md9egjnm02sk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbusername = "h1r174kgefa2bwv1";
+$dbpassword = "nz1tdqgzvtixcxel";
+$dbname = "dry1psur1w11ayhb";
+
+$conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+
+//2. Create the Query
+$sql = "select * from category";
+
+ //
+//3. Run the Query on the connection
+
+$result = mysqli_query($conn,$sql);
+
+//4. show the result
+while ($row = $result->fetch_assoc()){
+    echo $row["id"]." ".$row["name"];
+}
