eccfb27f87b96753aaf81ad7f9fba4b23679dc5a
kirteshm <mehtak06@myunitec.ac.nz>
2020-11-17 09:32:12 +1300

    trying links to connect to db

--- assessment2/registernow.php
+++ assessment2/registernow.php
@@ -7,30 +7,36 @@ $password = $_POST["password"];
 $address = $_POST["address"];
 $phonenumber = $_POST["phoneNumber"];
 
-//create database connection
+if ($firstname == "" || $lastname == "" || $username == "" || $password == "" || $address == "" || $phonenumber == ""){
 
-$server = "c584md9egjnm02sk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$dbusername = "h1r174kgefa2bwv1";
-$dbpassword = "nz1tdqgzvtixcxel";
-$dbname = "dry1psur1w11ayhb";
+    //create database connection
 
-$conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+    $server = "c584md9egjnm02sk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbusername = "h1r174kgefa2bwv1";
+    $dbpassword = "nz1tdqgzvtixcxel";
+    $dbname = "dry1psur1w11ayhb";
+
+    $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
 
 //creat a query
-$sql = "INSERT INTO `users`(`id`, `firstname`, `lastname`, `username`, `password`, `address`, `phoneNumber`) 
+    $sql = "INSERT INTO `users`(`id`, `firstname`, `lastname`, `username`, `password`, `address`, `phoneNumber`) 
 VALUES (NULL,'$firstname','$lastname','$username','$password','$address','$phonenumber')";
 
 //run the query
 
-if (mysqli_query($conn, $sql)){
-    echo "Welcome, You are now registered";
-    echo "<a href='index.php'>Back to Homepage</a>";
-}else{
-    echo "Please Try registering again with all correct details. Thank you";
+    if (mysqli_query($conn, $sql)){
+        echo "Welcome, You are now registered";
+        echo "<a href='index.php'>Back to Homepage</a>";
+    }else{
+        echo "Please Try registering again with all correct details. Thank you";
+    }
+
+
 }
 
-
-
+else{
+    echo "Please fill in all the details in the form"
+}
 
 
 
