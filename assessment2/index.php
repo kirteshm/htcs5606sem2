cc4ca5f9bb5a95e63011b4e5b1858fb8d30db9ef
kirteshm <mehtak06@myunitec.ac.nz>
2020-11-20 10:13:02 +1300

    finalising the website now

--- assessment2/index.php
+++ assessment2/index.php
@@ -53,8 +53,8 @@
           ?>
 
 
-          <h1><span style="color:maroon">Free Delivery</span></h1>
-          <h2><span style="color:maroon">for purchase over $300</span></h2>
+          <h1 align="center"><span style="color:maroon">Free Delivery</span></h1>
+          <h2 align="center"><span style="color:maroon">for purchase over $300</span></h2>
 
       </td>
     </tr>
