70989d805beb2bbbdecadf460823e2e0f3bb1e03
kirteshm <mehtak06@myunitec.ac.nz>
2020-11-19 17:13:07 +1300

    adding cart to page

--- assessment2/Cart.php
+++ assessment2/Cart.php
@@ -7,6 +7,13 @@ $orderedProductsQtys = $_SESSION["orderedProductQtys"];
 
 $i = 0;
 $delivery = 25; // default delivery charge
+echo "<table class='cartdetails' style='width: 90%'>
+            <tr>
+            <th>Product Name</th>
+            <th>Quantity</th>
+            <th>Price Each</th>
+            <th>Total Price</th>
+            </tr>";
 while ($i<sizeof($orderedProductsIDs)) {
 
     $orderedProductsID = $orderedProductsIDs[$i];
@@ -15,18 +22,26 @@ while ($i<sizeof($orderedProductsIDs)) {
     $price = getProductPriceByProductID($orderedProductsID);
     $Totalprice = $price * $orderedProductsQty;
 
-    echo "<p>Name: $productName and Qty: $orderedProductsQty Unit Price: $$price Total Price: $$Totalprice</p>";
+
+     echo   "<tr>
+            <td>$productName</td>
+            <td>$orderedProductsQty</td>
+            <td>$$price</td>
+            <td>$$Totalprice</td>
+            </tr>";
+
     $total = $total + ($price * $orderedProductsQty);
 
     $i++;
 }
-
+echo "</table>";
 //if the user is logged in than show form for checkout
 if (isset($_SESSION["userID"])){
 ?>
 
-<form action="checkout.php" method="post">
-    <textarea name="shipAddress" rows="10" cols="20" placeholder="Shipping Address if different to registered address"></textarea>
+<form class = cartform action="checkout.php" method="post">
+    <label>Shipping Address</label><br>
+    <textarea name="shipAddress" rows="8" cols="10" placeholder="If its different to registered"></textarea><br>
     <input type="submit" value="Checkout Now">
 </form>
 
