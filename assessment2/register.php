40dd305c0f85f3579ea5d1ccb49ab08ffdc80722
kirteshm <mehtak06@myunitec.ac.nz>
2020-11-22 13:43:04 +1300

    css styling

--- assessment2/register.php
+++ assessment2/register.php
@@ -21,7 +21,7 @@
                     <li><a class="active">Register Now</a></li>
                 </ul>
             </div>
-            <div class = 'w3-table-all' >
+
             <div class="regform">
                 <form method="post" action="registernow.php" class="contact-form">
                     <div class="row">
@@ -62,7 +62,7 @@
 
                 </form>
 
-            </div>
+
             </div>
 
             <div><img class="regpic" src="images/register.png" alt="Register pic"</div>
